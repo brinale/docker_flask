c111914feee7c3ed2b24093cccfae0b760b79e94
brinale <62750815+brinale@users.noreply.github.com>
2022-10-22 14:22:34 +0300

    final

--- app/app.py
+++ app/app.py
@@ -1,27 +1,27 @@
-from flask import Flask
+from flask import Flask, g
 import os
 import socket
 
-counter = 0
 app = Flask(__name__)
+counter = 0
 
 @app.route("/")
-def hello():
-    html = "<h3>Hello, Arina Belousova!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>"
+def show():
+    global counter
+    html = f"<p>{counter}</p>"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 @app.route("/about")
-def show():
-    html = "<p>{counter}</p>"
-    return html.format(name=os.getenv("NAME", "world"))
+def hello():
+    html = "<h3>Hello, Arina Belousova!</h3>" 
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 @app.route("/stat")
 def incr():
-    html = "<p>{counter}</p>"
-    counter=counter+1
-    return html.format(name=os.getenv("NAME", "world"))
-
+    global counter
+    html = f"<p>{counter}</p>"
+    counter = counter + 1
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
